865964bc77a111d1558bdb1f44acbce61a03720d
hometownzero <bradycpeters@gmail.com>
2017-07-24 18:07:35 -0500

    now queries google maps api, and returns the latitude and longitude of the address

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=5463%20lakewood%20terrace%20missouri',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
